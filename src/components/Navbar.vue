c9fd3aa5db975633868a8590148a3ff14eb3d28d
ReonLion <425699861@qq.com>
2019-06-10 17:37:49 +0800

    Develop

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
     <nav>
         <v-toolbar flat app>
-
+            <v-toolbar-title>
+                <span class="font-weight-light">Todo</span>
+                <span>Ninja</span>
+            </v-toolbar-title>
         </v-toolbar>
     </nav>
 </template>
