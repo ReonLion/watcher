1f0d846b81b2938d797a04e04b119a279d67ee78
ReonLion <ReonLion@users.noreply.github.com>
2019-06-14 00:38:28 +0800

    Dev

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,7 +2,55 @@
   <div class="HomePage">
     <h1 class="subheading grey--text">HomePage</h1>
     <v-container class="my-5">
-      content
+      <v-layout row wrap>
+        <v-flex xs12 md8>
+          <v-card flat class="pa-3">
+            <v-layout row wrap>
+              <v-flex>
+                <div class="caption grey--text">宿舍</div>
+                <div>狮城公寓639</div>
+              </v-flex>
+              <v-flex>
+                <div class="caption grey--text">更新时间</div>
+                <div>2019.06.14 00:29</div>
+              </v-flex>
+              <v-flex>
+                <div class="caption grey--text">在线人数</div>
+                <div>2 / 4</div>
+              </v-flex>
+              <v-flex>
+                <div class="caption grey--text">状态</div>
+                <div>coming</div>
+              </v-flex>
+            </v-layout>
+          </v-card>
+          <v-card flat class="pa-3">
+            <v-layout row wrap>
+              <v-flex>
+                <div class="caption grey--text">宿舍</div>
+                <div>狮城公寓639</div>
+              </v-flex>
+              <v-flex>
+                <div class="caption grey--text">更新时间</div>
+                <div>2019.06.14 00:29</div>
+              </v-flex>
+              <v-flex>
+                <div class="caption grey--text">在线人数</div>
+                <div>2 / 4</div>
+              </v-flex>
+              <v-flex>
+                <div class="caption grey--text">状态</div>
+                <div>coming</div>
+              </v-flex>
+            </v-layout>
+          </v-card>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-card color="red lighten-2" dark flat>
+            <v-card-text>Hello</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
   </div>
 </template>
